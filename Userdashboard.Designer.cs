5bf0fcf1acaa18c358600f04926a978dba63222c
Rach CyberShark <161412814+RachCyberShark@users.noreply.github.com>
2024-04-27 18:35:44 +0700

    Add files via upload

---
+++ Userdashboard.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace APP_RachST20.componnet
+{
+    partial class Userdashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subform = new System.Windows.Forms.Panel();
+            this.btn_Account = new System.Windows.Forms.Button();
+            this.btn_home = new System.Windows.Forms.Button();
+            this.btn_task = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // subform
+            // 
+            this.subform.BackColor = System.Drawing.Color.White;
+            this.subform.Location = new System.Drawing.Point(209, 24);
+            this.subform.Name = "subform";
+            this.subform.Size = new System.Drawing.Size(1044, 552);
+            this.subform.TabIndex = 17;
+            // 
+            // btn_Account
+            // 
+            this.btn_Account.Location = new System.Drawing.Point(24, 303);
+            this.btn_Account.Name = "btn_Account";
+            this.btn_Account.Size = new System.Drawing.Size(146, 55);
+            this.btn_Account.TabIndex = 18;
+            this.btn_Account.Text = "Account";
+            this.btn_Account.UseVisualStyleBackColor = true;
+            this.btn_Account.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_home
+            // 
+            this.btn_home.Location = new System.Drawing.Point(24, 190);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Size = new System.Drawing.Size(146, 55);
+            this.btn_home.TabIndex = 19;
+            this.btn_home.Text = "Home";
+            this.btn_home.UseVisualStyleBackColor = true;
+            this.btn_home.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_task
+            // 
+            this.btn_task.Location = new System.Drawing.Point(24, 416);
+            this.btn_task.Name = "btn_task";
+            this.btn_task.Size = new System.Drawing.Size(146, 55);
+            this.btn_task.TabIndex = 20;
+            this.btn_task.Text = "Cetecory";
+            this.btn_task.UseVisualStyleBackColor = true;
+            this.btn_task.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 55);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Book ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::APP_RachST20.Properties.Resources.c039b824474525_56334ce736de9;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Userdashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(1265, 605);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_task);
+            this.Controls.Add(this.btn_home);
+            this.Controls.Add(this.btn_Account);
+            this.Controls.Add(this.subform);
+            this.Name = "Userdashboard";
+            this.Text = "Userpf";
+            this.Load += new System.EventHandler(this.Userdashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel subform;
+        private System.Windows.Forms.Button btn_Account;
+        private System.Windows.Forms.Button btn_home;
+        private System.Windows.Forms.Button btn_task;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        public EventHandler Userpf_Load { get; set; }
+    }
+}
---
+++ index.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace APP_RachST20.componnet
+{
+    partial class index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(712, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PASSWORD";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(743, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "EMAIL";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.txtpassword.Location = new System.Drawing.Point(592, 369);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(414, 41);
+            this.txtpassword.TabIndex = 6;
+            this.txtpassword.Enter += new System.EventHandler(this.txtpassword_Enter);
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.txtemail.Location = new System.Drawing.Point(592, 214);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(414, 41);
+            this.txtemail.TabIndex = 5;
+            this.txtemail.TextChanged += new System.EventHandler(this.txtusername_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button2.Location = new System.Drawing.Point(742, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 42);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button3.Location = new System.Drawing.Point(742, 444);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 53);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Login";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::APP_RachST20.Properties.Resources.c039b824474525_56334ce736de9;
+            this.pictureBox1.Location = new System.Drawing.Point(-39, -81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(607, 758);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(633, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(336, 36);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Admin Login book Shop";
+            // 
+            // index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(1026, 605);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtemail);
+            this.Name = "index";
+            this.Text = "index";
+            this.Load += new System.EventHandler(this.index_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
